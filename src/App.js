7faecf0f4e82db7e7cfd9ac0cb18bf0edad7c290
Samuellsousa <99738166+Samuellsousa@users.noreply.github.com>
2023-03-16 15:49:37 -0300

    crud de links

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import {createBrowserRouter} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Admin from './pages/Admin'
+import Networks from './components/Networks'
+
+import Error from './pages/Error'
+
+import Private from './routes/Private'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Home/>
+  },
+  {
+    path:'/login',
+    element: <Login/>
+  },
+  {
+    path:'/admin',
+    element: <Private> <Admin/> </Private>
+  },
+  {
+    path: '/admin/social',
+    element:<Private> <Networks/> </Private>
+  },
+  {
+    path:'*',
+    element:<Error/>
+  }
+])
+
+export { router };
+
